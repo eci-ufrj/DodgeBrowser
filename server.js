d6fb4d95a128c12bdf272eb2d0271a43eaaa7ade
viniciusmotta4 <viniciusnmotta84@gmail.com>
2014-05-22 21:33:50 -0300

    Initial external js files

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ console.log("Running on port "+PORT);
 
 
 // app.use("/components",express.static(__dirname+"/bower_components"));
-app.use("/libs",express.static(__dirname+"/static/js/libs/bower_components"));
-app.use("/",express.static(__dirname+"/templates/"))
-
+app.use("/libs/bower",express.static(__dirname+"/static/js/libs/bower_components"));
+app.use("/",express.static(__dirname+"/templates/"));
+app.use("/libs/js", express.static(__dirname+"/static/js/libs"))
 
 
 
